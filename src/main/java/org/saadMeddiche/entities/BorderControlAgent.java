7c60b33649e11f4a494c2687c030161f93b1055a
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-01-02 17:55:36 +0100

    enable SquareHandler, Yay it worked; I received ny first message

--- src/main/java/org/saadMeddiche/entities/BorderControlAgent.java
+++ src/main/java/org/saadMeddiche/entities/BorderControlAgent.java
@@ -79,8 +79,8 @@ public class BorderControlAgent extends Thread {
             out.write(confirmationResponse);
             out.flush();
 
-//            SquareHandler squareHandler = new SquareHandler(socket);
-//            squareHandler.listen();
+            SquareHandler squareHandler = new SquareHandler(socket);
+            squareHandler.listen();
 
             LOG.info("[{}]'s passport is accepted response, his name is {}", socket, visitorInfo.fullName);
 
